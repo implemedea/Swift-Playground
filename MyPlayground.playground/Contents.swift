d4bc7e55e53fa97501a04928a1e829465d5f0811
implemedea <36766158+implemedea@users.noreply.github.com>
2020-09-16 18:52:21 +0530

    initial commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,232 @@
+import Cocoa
+
+var str = "Hello, playground"
+
+
+/// Property observers
+struct Temprature {
+    var celcius: Double
+    var tempFarenheat: Double {
+        get {
+           return (celcius * 9/5 + 32)
+        }
+        set(newFarenheatValue) {
+            celcius = (newFarenheatValue - 32) * 5/9
+        }
+    }
+}
+
+var obj = Temprature(celcius: 0.0)
+obj.tempFarenheat
+obj.celcius
+
+class StepCounter {
+    var totalSteps: Int = 0 {
+        willSet(newTotalSteps) {
+            print("About to set totalSteps to \(newTotalSteps)")
+        }
+        didSet {
+            if totalSteps > oldValue  {
+                print("Added \(totalSteps - oldValue) steps")
+            }
+        }
+    }
+}
+
+var step = StepCounter()
+step.totalSteps = 10
+step.totalSteps = 100
+
+/********************************************************/
+
+/// Error enum
+enum PasswordValidationError: Error {
+    case noCapitalletter
+    case noNumber
+    case notEnoughCharacter(length: Int)
+}
+
+class ErrorHandling {
+    
+    /// Handling errors internaly
+    func callHandleErrorInternal() {
+        handleErrorInternal(password: "asdasdasd")
+    }
+    
+    /// Handling thrown error
+    func callThrowError() {
+        do {
+            let notError = try throwError(password: "asd123asd")
+            print(notError ? "Password success" : "Password failed")
+        } catch PasswordValidationError.noCapitalletter {
+            print("noCapitalletter")
+        } catch PasswordValidationError.noNumber {
+            print("noNumber")
+        } catch PasswordValidationError.notEnoughCharacter(let length) {
+            print(length)
+        } catch {
+            print("unexpected error")
+        }
+    }
+    
+    func treatErrorOptional() {
+        let notError = try? throwError(password: "asd123asd")
+        if notError == nil {
+            print("Error thrown")
+        } else {
+            print("password success")
+        }
+    }
+    
+    /// Handling only specific error
+    func callThrowUnExpectedError() {
+        do {
+            try throwUnExpectedError()
+        } catch let error as NSError  {
+            print(error)
+        }
+    }
+    
+    /// Throw only unexpected error and handle all other errors
+    /// - Throws: throw only unexpected error
+    /// - Returns: password strength
+    func throwUnExpectedError() throws -> Bool {
+        do {
+            throw NSError(domain: "com.test.error", code: 401, userInfo: ["error":"UnExpectedError"])
+        } catch PasswordValidationError.noCapitalletter {
+            print("noCapitalletter")
+            return false
+        } catch PasswordValidationError.noNumber {
+            print("noNumber")
+            return false
+        } catch PasswordValidationError.notEnoughCharacter(let length) {
+            print(length)
+            return false
+        }
+    }
+    
+    /// Throw errors
+    /// - Parameter password: password
+    /// - Throws: throw errors
+    /// - Returns: password strength
+    func throwError(password: String) throws -> Bool {
+        if password == "asd123asd" {
+            throw PasswordValidationError.noCapitalletter
+        } else if password == "asdasdasd" {
+            throw PasswordValidationError.noNumber
+        } else if password == "asd" {
+            throw PasswordValidationError.notEnoughCharacter(length: 4)
+        } else {
+            return true
+        }
+    }
+    
+    /// Handle error internally not throw error
+    /// - Parameter password: password
+    /// - Returns: password strength
+    func handleErrorInternal(password: String) -> Bool {
+        do {
+            if password == "asd123asd" {
+                throw PasswordValidationError.noCapitalletter
+            } else if password == "asdasdasd" {
+                throw PasswordValidationError.noNumber
+            } else if password == "asd" {
+                throw PasswordValidationError.notEnoughCharacter(length: 4)
+            } else {
+                return true
+            }
+        } catch let error as NSError {
+            print(error)
+            return false
+        }
+    }
+    
+    func callDivide() {
+        do {
+            let result =  try divide(value1: 10, value2: 0)
+            print(result)
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func divide(value1: Int, value2: Int) throws -> Double {
+        guard value2 != 0 else {
+            throw NSError(domain: "com.error.zero", code: 101, userInfo: [:])
+        }
+        return Double(value1/value2)
+    }
+}
+
+let error = ErrorHandling()
+error.callThrowError()
+error.callHandleErrorInternal()
+error.callThrowUnExpectedError()
+error.treatErrorOptional()
+error.callDivide()
+
+/********************************************************/
+
+/// Type method - Reference type
+class Maths1 {
+    class func addNumber(a: Int, b: Int) ->  Int {
+        return a+b
+    }
+}
+print(Maths1.addNumber(a: 1, b: 2))
+
+
+/// Type method - value type
+enum Maths2 {
+    case Add
+    case Subtract
+    case Multiply
+    case Divide
+    
+    static func addNumber(a: Int, b: Int) ->  Int {
+        return a+b
+    }
+}
+print(Maths2.addNumber(a: 1, b: 2))
+
+/********************************************************/
+
+/// Mutating function in swift
+
+struct Maths3 {
+    var a: Int = 0
+    var b: Int = 0
+    var c: Int = 0
+    
+    mutating func addition(a: Int, b: Int) {
+        c = a+b
+    }
+}
+
+
+var add = Maths3(a: 0, b: 0, c: 0)
+add.addition(a: 1, b: 1)
+print(add.c, add.a, add.b)
+
+/********************************************************/
+
+/// Subscript
+class Day {
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    
+    subscript (index: Int) -> String {
+        get {
+           return days[index]
+        }
+        set(newValue) {
+            self.days[index] = newValue
+        }
+    }
+}
+
+let objDay = Day()
+print(objDay.days[0])
+objDay.days[0] = "Monday"
+print(objDay.days)
+
+/********************************************************/
